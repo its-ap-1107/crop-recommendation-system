5bfbc76bded91462506ddb2e6cddba4e442c88eb
githubdhiraj <dhirajlaulkar101@gmail.com>
2025-02-22 19:49:46 +0530

    feat:card comp

---
+++ frontend/src/components/Card.jsx
@@ -0,0 +1,15 @@
+
+import PropTypes from 'prop-types'
+
+export function Card({ children, className = "" }) {
+  return (
+    <div className={`bg-white rounded-lg shadow-lg ${className}`}>
+      {children}
+    </div>
+  )
+}
+
+Card.propTypes = {
+  children: PropTypes.node.isRequired,
+  className: PropTypes.string
+} 
